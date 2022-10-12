d0a3326be7b6fe4a604cc4d941f3b535f5dbf390
jcollins-pd <108346904+jcollins-pd@users.noreply.github.com>
2022-10-12 13:19:58 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -116,28 +116,28 @@ resource "pagerduty_service" "unrouted" {
 
 # SERVICE INTEGRATION
 # see https://registry.terraform.io/providers/PagerDuty/pagerduty/latest/docs/data-sources/vendor
-data "pagerduty_vendor" "cloudwatch" {
-  name = "Cloudwatch"
-}
+#data "pagerduty_vendor" "cloudwatch" {
+#  name = "Cloudwatch"
+#}
 
 # see https://registry.terraform.io/providers/PagerDuty/pagerduty/latest/docs/resources/service_integration
-resource "pagerduty_service_integration" "cloudwatch" {
-  name    = data.pagerduty_vendor.cloudwatch.name
-  service = pagerduty_service.api.id
-  vendor  = data.pagerduty_vendor.cloudwatch.id
-}
+#resource "pagerduty_service_integration" "cloudwatch" {
+#  name    = data.pagerduty_vendor.cloudwatch.name
+#  service = pagerduty_service.api.id
+#  vendor  = data.pagerduty_vendor.cloudwatch.id
+#}
 
 # see https://registry.terraform.io/providers/PagerDuty/pagerduty/latest/docs/data-sources/vendor
-data "pagerduty_vendor" "datadog" {
-  name = "Datadog"
-}
-
+#data "pagerduty_vendor" "datadog" {
+#  name = "Datadog"
+#}
+#
 # see https://registry.terraform.io/providers/PagerDuty/pagerduty/latest/docs/resources/service_integration
-resource "pagerduty_service_integration" "datadog" {
-  name    = data.pagerduty_vendor.datadog.name
-  service = pagerduty_service.api.id
-  vendor  = data.pagerduty_vendor.datadog.id
-}
+#resource "pagerduty_service_integration" "datadog" {
+#  name    = data.pagerduty_vendor.datadog.name
+#  service = pagerduty_service.api.id
+#  vendor  = data.pagerduty_vendor.datadog.id
+#}
 
 # BUSINESS SERVICE
 # see https://registry.terraform.io/providers/PagerDuty/pagerduty/latest/docs/resources/business_service
